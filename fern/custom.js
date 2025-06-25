282a4a83dd17ebd407e5058a4b1d38f3ba20bd19
Ulysse Carion <ulysse.carion@ssoready.com>
2025-06-25 15:26:00 -0700

    update snippet

--- fern/custom.js
+++ fern/custom.js
@@ -28,3 +28,6 @@ function gtag() {
 }
 gtag("js", new Date());
 gtag("config", "G-HW27FX565S");
+
+// rb2b
+!function () {var reb2b = window.reb2b = window.reb2b || []; if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"]; reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments); args.unshift(method);reb2b.push(args);return reb2b;};}; for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);} reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true; script.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/QO92DH7QXLN7.js.gz"; var first = document.getElementsByTagName("script")[0]; first.parentNode.insertBefore(script, first);}; reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("QO92DH7QXLN7");}();
